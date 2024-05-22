68294d5b6d23b2f73c5cf37aa42b76f084eb2bf1
Tim McConechy <tim.mcconechy@infor.com>
2024-05-22 13:26:27 -0400

    Add combined demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,9 @@ import {
   SohoModuleNavContainerComponent,
   SohoModuleNavSwitcherComponent,
   SohoModuleNavSettingsComponent,
-  SohoModuleNavComponent
+  SohoModuleNavComponent,
+  SohoDataGridComponent,
+  SohoInputComponent
 } from 'ids-enterprise-ng';
 
 const defaultRoles: Array<SohoModuleNavSwitcherRoleRecord> = [
@@ -39,6 +41,7 @@ const defaultRoles: Array<SohoModuleNavSwitcherRoleRecord> = [
 export class AppComponent implements AfterViewInit {
   @ViewChild(SohoModuleNavSwitcherComponent) moduleNavSwitcher?: SohoModuleNavSwitcherComponent;
   @ViewChild(SohoModuleNavContainerComponent) moduleNavContainer?: SohoModuleNavContainerComponent;
+  @ViewChild(SohoDataGridComponent, { static: true }) dataGrid?: SohoDataGridComponent;
 
   /**
    * Local Storage Key
@@ -61,6 +64,16 @@ export class AppComponent implements AfterViewInit {
 
   public personalizeOptions: SohoPersonalizeOptions = {};
 
+  public options!: SohoDataGridOptions;
+  public newRowData!: string;
+  public tooltipOptions: SohoTooltipOptions = {
+    placement: 'top'
+  }
+
+  public tooltipCompressor: SohoTooltipOptions = {
+    placement: 'bottom'
+  }
+
   constructor(private readonly renderLoop: SohoRenderLoopService) {
     // Init render loop manually for Angular applications
     // Ensures requestAnimationFrame is running outside of Angular Zone
@@ -71,6 +84,10 @@ export class AppComponent implements AfterViewInit {
     displayMode: 'collapsed',
     selectedRole: 'admin',
     roles: defaultRoles
+  }
+
+  ngOnInit(): void {
+    this.setGridOptions();
   }
 
   ngAfterViewInit(): void {
@@ -88,5 +105,65 @@ export class AppComponent implements AfterViewInit {
     this.useNewIcons = ev.data.theme === 'theme-new-light'
       || ev.data.theme === 'theme-new-dark'
       || ev.data.theme === 'theme-new-contrast';
+  }
+
+  private setGridOptions(): void {
+    const columns = [
+      {
+        id: 'name',
+        name: 'Name',
+        field: 'name',
+        // width: 80,
+        sortable: true,
+        headerTooltip: 'Name Column',
+        tooltipOptions: this.tooltipOptions,
+        formatter: Soho.Formatters.Ellipsis,
+      },
+      {
+        id: 'compressor',
+        name: 'Compressor',
+        field: 'compressor',
+        // width: 80,
+        sortable: true,
+        headerTooltip: 'Compressor Column',
+        tooltipOptions: this.tooltipCompressor,
+        formatter: Soho.Formatters.Ellipsis,
+      },
+      {
+        id: 'quantity',
+        name: 'Quantity',
+        field: 'quantity',
+        // width: 80,
+        sortable: true,
+        headerTooltip: 'Quantity Column',
+        tooltipOptions: this.tooltipOptions,
+        formatter: Soho.Formatters.Ellipsis,
+      },
+      {
+        id: 'open',
+        name: 'Actions',
+        // width: 80,
+        sortable: false,
+        formatter: Soho.Formatters.Button,
+        icon: 'info',
+        menuId: 'card-options',
+        click: (_: Event, data: SohoDataGridColumnClickData[]) => {
+          console.info(data);
+          console.info(data[0].item);
+        },
+      },
+    ];
+    this.options = {
+      dataset: [{ name: 'Lorem', compressor: 'Compressor 1', quantity: 24 }, { name: 'Foo', compressor: 'Compressor 2', quantity: 22 }],
+      columns,
+      enableTooltips: true
+    };
+  }
+
+  count = 0;
+
+  addRow() {
+    this.count++;
+    this.dataGrid?.addRow({ name: `New Row ${this.count}` }, 'bottom');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import {SohoButtonModule, SohoComponentsModule, SohoLocaleModule} from 'ids-enterprise-ng';
+import { SohoButtonModule, SohoComponentsModule, SohoLocaleModule } from 'ids-enterprise-ng';
 
 import { AppComponent } from './app.component';
 import { SohoLocaleInitializerModule } from './locale/soho-locale-initializer.module';
@@ -15,11 +16,12 @@ import { PersonalizeMenuComponent } from './personalize-menu/personalize-menu.co
     PersonalizeMenuComponent
   ],
   imports: [
-      BrowserModule,
-      SohoLocaleModule,
-      SohoButtonModule,
-      SohoLocaleInitializerModule,
-      SohoComponentsModule
+    BrowserModule,
+    SohoLocaleModule,
+    SohoButtonModule,
+    SohoLocaleInitializerModule,
+    SohoComponentsModule,
+    FormsModule
   ],
   providers: [
     {
